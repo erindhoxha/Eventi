8be6c5cdc2a143705793dbfb4f7613333da03fe3
erindhoxha <hello@erindhoxha.dev>
2023-10-08 13:55:25 +1100

    Snap to alignment

--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import { Box, Text as MagnusText } from 'react-native-magnus';
 
-const RestaurantCard = ({ item }) => {
- return (
-  <Box w={300} mr="xl">
-   <Box
-    rounded="xl"
-    h={150}
-    bgImg={{
-     uri: item.image,
-    }}
-   >
-    <Box
-     bg="pink500"
-     rounded="md"
-     row
-     flexWrap="wrap"
-     px="md"
-     m="lg"
-     alignSelf="flex-start"
-    >
-     <MagnusText color="white" fontSize="sm">
-      2 Rooms
-     </MagnusText>
+const RestaurantCard = ({ item, width }) => {
+  return (
+    <Box w={width} mx="lg">
+      <Box
+        rounded="xl"
+        h={150}
+        bgImg={{
+          uri: item.image,
+        }}
+      >
+        <Box
+          bg="pink500"
+          rounded="md"
+          row
+          flexWrap="wrap"
+          px="md"
+          m="lg"
+          alignSelf="flex-start"
+        >
+          <MagnusText color="white" fontSize="sm">
+            2 Rooms
+          </MagnusText>
+        </Box>
+      </Box>
+      <Box row alignItems="center">
+        <Box flex={1}>
+          <MagnusText fontWeight="bold" fontSize="xl" mt="sm">
+            Sunny Apartment
+          </MagnusText>
+          <MagnusText color="gray500" fontSize="sm">
+            Gurgoan, India
+          </MagnusText>
+        </Box>
+        <Box row alignItems="center">
+          <MagnusText color="blue500" fontWeight="bold" fontSize="xl">
+            $500
+          </MagnusText>
+          <MagnusText color="gray500" ml="md">
+            / per day
+          </MagnusText>
+        </Box>
+      </Box>
     </Box>
-   </Box>
-   <Box row alignItems="center">
-    <Box flex={1}>
-     <MagnusText fontWeight="bold" fontSize="xl" mt="sm">
-      Sunny Apartment
-     </MagnusText>
-     <MagnusText color="gray500" fontSize="sm">
-      Gurgoan, India
-     </MagnusText>
-    </Box>
-    <Box row alignItems="center">
-     <MagnusText color="blue500" fontWeight="bold" fontSize="xl">
-      $500
-     </MagnusText>
-     <MagnusText color="gray500" ml="md">
-      / per day
-     </MagnusText>
-    </Box>
-   </Box>
-  </Box>
- );
+  );
 };
 
 export default RestaurantCard;
--- src/screens/FoodScreen.js
+++ src/screens/FoodScreen.js
@@ -1,113 +1,142 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, FlatList, Pressable } from 'react-native';
 import {
- Text as MagnusText,
- ThemeProvider,
- Button,
- Input,
- Icon,
- Host,
- Fab,
- Portal,
- Box,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Pressable,
+  Dimensions,
+} from 'react-native';
+import {
+  Text as MagnusText,
+  ThemeProvider,
+  Button,
+  Input,
+  Icon,
+  Host,
+  Fab,
+  Portal,
+  Box,
 } from 'react-native-magnus';
 import SearchBar from '../components/SearchBar/SearchBar';
 import RestaurantCard from '../components/RestaurantCard/RestaurantCard';
 
 const friends = [
- {
-  id: 1,
-  image:
-   'https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
- },
- {
-  id: 2,
-  image:
-   'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1927&q=80',
- },
- {
-  id: 3,
-  image:
-   'https://images.unsplash.com/photo-1516640997890-5e4c83df8419?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
- },
- {
-  id: 4,
-  image:
-   'https://images.unsplash.com/photo-1516467508483-a7212febe31a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80',
- },
- {
-  id: 5,
-  image:
-   'https://images.unsplash.com/photo-1453365607868-7deed8cc7d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
- },
- {
-  id: 6,
-  image:
-   'https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
- },
+  {
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1927&q=80',
+  },
+  {
+    id: 3,
+    image:
+      'https://images.unsplash.com/photo-1516640997890-5e4c83df8419?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    id: 4,
+    image:
+      'https://images.unsplash.com/photo-1516467508483-a7212febe31a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80',
+  },
+  {
+    id: 5,
+    image:
+      'https://images.unsplash.com/photo-1453365607868-7deed8cc7d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    id: 6,
+    image:
+      'https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
 ];
 
+const { width, height } = Dimensions.get('window');
+const ITEM_SIZE = width * 0.82;
+const SPACING = 24;
+
 const FoodScreen = () => {
- return (
-  <ThemeProvider>
-   <StatusBar barStyle="dark-content" />
-   <SafeAreaView style={{ flex: 1 }}>
-    <Host>
-     <Box mx="xl" flex={1} pt="lg">
-      <MagnusText color="dark" fontWeight="bold" fontSize="4xl" mt="md">
-       Search for food
-      </MagnusText>
-      <SearchBar />
-      <MagnusText color="dark" fontWeight="bold" fontSize="2xl" mt="xl" mb="md">
-       In your area
-      </MagnusText>
-      <Box flexGrow={1}>
-       <FlatList
-        horizontal
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-        data={friends}
-        renderItem={({ item }) => <RestaurantCard item={item} />}
-        keyExtractor={(item) => `friend-list-item-${item.id}`}
-       />
-      </Box>
-     </Box>
-     <Portal>
-      <Fab bg="blue600" h={50} w={50}>
-       <Button p="none" bg="transparent" justifyContent="flex-end">
-        <Box rounded="sm" bg="white" p="sm">
-         <MagnusText fontSize="MagnusText100">Cheer</MagnusText>
-        </Box>
-        <Icon
-         name="user"
-         color="blue600"
-         h={50}
-         w={50}
-         rounded="circle"
-         ml="md"
-         bg="white"
-        />
-       </Button>
-       <Button p="none" bg="transparent" justifyContent="flex-end" disabled>
-        <Box rounded="sm" bg="white" p="sm">
-         <MagnusText fontSize="MagnusText100">Boost</MagnusText>
-        </Box>
-        <Icon
-         name="user"
-         color="blue600"
-         h={50}
-         w={50}
-         rounded="circle"
-         ml="md"
-         bg="white"
-        />
-       </Button>
-      </Fab>
-     </Portal>
-    </Host>
-   </SafeAreaView>
-  </ThemeProvider>
- );
+  return (
+    <ThemeProvider>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Host>
+          <Box flex={1} pt="lg">
+            <Box mx="lg">
+              <MagnusText color="dark" fontWeight="bold" fontSize="4xl" mt="md">
+                Search for food
+              </MagnusText>
+              <SearchBar />
+              <MagnusText
+                color="dark"
+                fontWeight="bold"
+                fontSize="2xl"
+                mt="xl"
+                mb="md"
+              >
+                In your area
+              </MagnusText>
+            </Box>
+            <Box flexGrow={1}>
+              <FlatList
+                snapToAlignment="center"
+                snapToInterval={ITEM_SIZE + SPACING}
+                decelerationRate="fast"
+                bounces={false}
+                horizontal
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                data={friends}
+                renderItem={({ item }) => (
+                  <RestaurantCard width={ITEM_SIZE} item={item} />
+                )}
+                keyExtractor={(item) => `friend-list-item-${item.id}`}
+              />
+            </Box>
+          </Box>
+          <Portal>
+            <Fab bg="blue600" h={50} w={50}>
+              <Button p="none" bg="transparent" justifyContent="flex-end">
+                <Box rounded="sm" bg="white" p="sm">
+                  <MagnusText fontSize="MagnusText100">Cheer</MagnusText>
+                </Box>
+                <Icon
+                  name="user"
+                  color="blue600"
+                  h={50}
+                  w={50}
+                  rounded="circle"
+                  ml="md"
+                  bg="white"
+                />
+              </Button>
+              <Button
+                p="none"
+                bg="transparent"
+                justifyContent="flex-end"
+                disabled
+              >
+                <Box rounded="sm" bg="white" p="sm">
+                  <MagnusText fontSize="MagnusText100">Boost</MagnusText>
+                </Box>
+                <Icon
+                  name="user"
+                  color="blue600"
+                  h={50}
+                  w={50}
+                  rounded="circle"
+                  ml="md"
+                  bg="white"
+                />
+              </Button>
+            </Fab>
+          </Portal>
+        </Host>
+      </SafeAreaView>
+    </ThemeProvider>
+  );
 };
 
 export default FoodScreen;
