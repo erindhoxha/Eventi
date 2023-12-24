ce6a6ee709d0eebd33cdd6ab451706a4dcb71756
erindhoxha <erind.cbh@gmail.com>
2023-12-24 16:30:44 +1100

    Added tab bar

--- App.js
+++ App.js
@@ -1,22 +1,67 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import FoodScreen from './src/screens/FoodScreen';
-import { Text } from 'react-native-magnus';
+import { Icon, Text } from 'react-native-magnus';
 
-const navigator = createStackNavigator(
-  {
-    Food: FoodScreen,
+const FoodStack = createStackNavigator(
+ {
+  Food: FoodScreen,
+  Home: FoodScreen,
+ },
+ {
+  defaultNavigationOptions: {
+   headerTintColor: '#fff',
+   headerStyle: {
+    backgroundColor: '#000',
+   },
+   title: 'Business search',
   },
-  {
-    initialRouteName: 'Food',
-    defaultNavigationOptions: {
-      headerTintColor: '#fff',
-      headerStyle: {
-        backgroundColor: '#000',
-      },
-      title: 'Business search',
-    },
-  }
+ }
 );
 
-export default createAppContainer(navigator);
+const TabNavigator = createBottomTabNavigator(
+ {
+  Home: FoodStack,
+  Bookmarks: FoodScreen,
+ },
+ {
+  tabBarOptions: {
+   safeAreaInset: { bottom: 24 },
+   activeTintColor: '#fff',
+   labelStyle: {
+    display: 'none',
+   },
+   style: {
+    backgroundColor: '#000',
+    padding: 24,
+   },
+  },
+  defaultNavigationOptions: ({ navigation }) => ({
+   tabBarIcon: ({ focused, horizontal, tintColor }) => {
+    const { routeName } = navigation.state;
+    let icon = 'home';
+    if (routeName === 'Bookmarks') {
+     icon = 'book';
+    }
+    return (
+     <>
+      <Icon
+       focused={focused}
+       name={icon}
+       backgroundColor="#000"
+       color={tintColor}
+       fontSize={24}
+       fontFamily="FontAwesome"
+      />
+      <Text mt="sm" color={tintColor}>
+       {routeName}
+      </Text>
+     </>
+    );
+   },
+  }),
+ }
+);
+
+export default createAppContainer(TabNavigator);
--- src/screens/FoodScreen.tsx
+++ src/screens/FoodScreen.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  StatusBar,
-  NativeSyntheticEvent,
-  TextInputChangeEventData,
-  ActivityIndicator,
-  View,
-  ScrollView,
+ SafeAreaView,
+ StatusBar,
+ NativeSyntheticEvent,
+ TextInputChangeEventData,
+ ActivityIndicator,
+ View,
+ ScrollView,
 } from 'react-native';
 import {
-  Text as MagnusText,
-  ThemeProvider,
-  Host,
-  Box,
+ Text as MagnusText,
+ ThemeProvider,
+ Host,
+ Box,
 } from 'react-native-magnus';
 import SearchBar from '../components/SearchBar/SearchBar';
 import useResults from '../hooks/useResults';
@@ -21,97 +21,101 @@ import RestaurantList from '../components/RestaurantList/RestaurantList';
 import HorizontalLine from '../components/HorizontalLine/HorizontalLine';
 
 const FoodScreen = () => {
-  const [value, setValue] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
-    setValue(e.nativeEvent.text);
-  };
-  const { results, request, error, loading } = useResults();
+ const [value, setValue] = useState('');
+ const [searchTerm, setSearchTerm] = useState('');
+ const onChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+  setValue(e.nativeEvent.text);
+ };
+ const { results, request, error, loading } = useResults();
 
-  const onSubmit = async (term: string) => {
-    setSearchTerm(term);
-    request(term);
-  };
+ const onSubmit = async (term: string) => {
+  setSearchTerm(term);
+  request(term);
+ };
 
-  useEffect(() => {
-    request('pizza');
-  }, []);
+ const budgetFriendlyResults = results.filter((result) => {
+  return result.price === '$';
+ });
 
-  return (
-    <ThemeProvider>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView style={{ flex: 1 }}>
-        <Host>
-          <ScrollView>
-            <Box flex={1} pt="lg">
-              {loading ? (
-                <Box m="lg">
-                  <ActivityIndicator />
-                </Box>
-              ) : null}
-              <Box mx="lg">
-                <MagnusText
-                  color="dark"
-                  fontWeight="bold"
-                  fontSize="xl"
-                  mt="md"
-                  mb="md"
-                >
-                  Local goodies, all in one place
-                </MagnusText>
-                <View
-                  style={{
-                    borderBottomColor: 'black',
-                    borderBottomWidth: 1,
-                  }}
-                />
-              </Box>
-              <Box mx="lg" mb="lg">
-                <MagnusText
-                  color="gray800"
-                  fontWeight="500"
-                  fontSize="sm"
-                  mt="md"
-                >
-                  Search for food or restaurant name
-                </MagnusText>
-                <SearchBar onChange={onChange} onSubmit={onSubmit} />
-                {error && (
-                  <MagnusText color="red500" fontSize="md" mt="sm">
-                    Something went wrong on our end. Please try again.
-                  </MagnusText>
-                )}
-              </Box>
+ const classyResults = results.filter((result) => {
+  return result.price === '$$';
+ });
 
-              {results && results.length > 0 && (
-                <RestaurantList title={`In your area`} results={results} />
-              )}
+ useEffect(() => {
+  request('pizza');
+ }, []);
 
-              <HorizontalLine fade />
+ return (
+  <ThemeProvider>
+   <StatusBar barStyle="light-content" />
+   <Host>
+    <ScrollView>
+     <Box flex={1} pt="lg">
+      {loading ? (
+       <Box m="lg">
+        <ActivityIndicator />
+       </Box>
+      ) : null}
+      <Box mx="lg">
+       <MagnusText
+        color="dark"
+        fontWeight="bold"
+        fontSize="4xl"
+        mt="md"
+        mb="md"
+       >
+        Local goodies, all in one place
+       </MagnusText>
+       <View
+        style={{
+         borderBottomColor: 'black',
+         borderBottomWidth: 1,
+        }}
+       />
+      </Box>
+      <Box mx="lg" mb="lg">
+       <MagnusText color="gray800" fontWeight="500" fontSize="sm" mt="md">
+        Search for food or restaurant name
+       </MagnusText>
+       <SearchBar onChange={onChange} onSubmit={onSubmit} />
+       {error && (
+        <MagnusText color="red500" fontSize="md" mt="sm">
+         Something went wrong on our end. Please try again.
+        </MagnusText>
+       )}
+      </Box>
 
-              {results && results.length > 0 && (
-                <RestaurantList title="Budget friendly" results={results} />
-              )}
+      {results && results.length > 0 && (
+       <RestaurantList title={`In your area`} results={results} />
+      )}
 
-              <HorizontalLine fade />
+      <HorizontalLine fade />
 
-              {results && results.length > 0 && (
-                <RestaurantList title="Classy Sundays" results={results} />
-              )}
-              <Box m="lg">
-                {results && results.length ? (
-                  <MagnusText fontWeight="normal" fontSize="md" mt="md">
-                    We have found {results.length} results
-                  </MagnusText>
-                ) : null}
-              </Box>
-            </Box>
-          </ScrollView>
-          <OptionsPortal />
-        </Host>
-      </SafeAreaView>
-    </ThemeProvider>
-  );
+      {results && results.length > 0 && (
+       <RestaurantList
+        title="Budget friendly"
+        results={budgetFriendlyResults}
+       />
+      )}
+
+      <HorizontalLine fade />
+
+      {results && results.length > 0 && (
+       <RestaurantList title="Classy Sundays" results={classyResults} />
+      )}
+      <Box m="lg">
+       {results && results.length ? (
+        <MagnusText fontWeight="normal" fontSize="md" mt="md">
+         We have found {results.length} results
+        </MagnusText>
+       ) : null}
+      </Box>
+     </Box>
+    </ScrollView>
+    <OptionsPortal />
+   </Host>
+  </ThemeProvider>
+ );
 };
 
 export default FoodScreen;
