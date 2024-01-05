8a48ee96f35afb5abae24c05a196d0bde4053a0e
erindhoxha <erind.cbh@gmail.com>
2024-01-05 15:36:26 +1100

    Added supabase login feature

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { ElementRef, useRef, useState } from "react";
 import {
+  Alert,
   Platform,
   SafeAreaView,
   ScrollView,
@@ -16,9 +17,46 @@ import {
   ThemeProvider,
 } from "react-native-magnus";
 import type { RoutePropWithParams } from "../../App";
+import { supabase } from "../../lib/supabase";
+import { TextInput } from "react-native-gesture-handler";
 
 const LoginScreen = ({ route }: { route: RoutePropWithParams<"Login"> }) => {
   const [submitted, setSubmitted] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
+  }
+
   return (
     <ThemeProvider>
       <StatusBar barStyle="light-content" />
@@ -75,16 +113,38 @@ const LoginScreen = ({ route }: { route: RoutePropWithParams<"Login"> }) => {
                     Email address
                   </MagnusText>
                   <Input
-                    hitSlop={{ top: 24, bottom: 24, left: 24, right: 24 }}
+                    ref={emailRef}
+                    value={email}
+                    p={10}
+                    fontSize={16}
                     keyboardType="email-address"
                     inputMode="email"
                     placeholder="Email address"
                     autoComplete="email"
                     autoFocus
+                    autoCapitalize="none"
+                    focusBorderColor="blue700"
+                    hitSlop={{ top: 24, bottom: 24, left: 24, right: 24 }}
+                    onChangeText={(text) => setEmail(text)}
+                    onSubmitEditing={() => {
+                      passwordRef.current?.focus();
+                    }}
+                  />
+                  <Input
+                    ref={passwordRef}
+                    value={password}
+                    hitSlop={{ top: 24, bottom: 24, left: 24, right: 24 }}
+                    keyboardType="default"
+                    inputMode="text"
+                    placeholder="Password"
+                    autoComplete="password"
+                    secureTextEntry
                     fontSize={16}
                     autoCapitalize="none"
+                    mt="lg"
                     p={10}
                     focusBorderColor="blue700"
+                    onChangeText={(text) => setPassword(text)}
                     onSubmitEditing={() => {
                       setSubmitted(true);
                     }}
@@ -94,19 +154,30 @@ const LoginScreen = ({ route }: { route: RoutePropWithParams<"Login"> }) => {
                     mt="lg"
                     bg="green700"
                     color="white"
+                    disabled={loading}
                     onPress={() => {
-                      setSubmitted(true);
+                      signInWithEmail().finally(() => {
+                        setSubmitted(true);
+                      });
                     }}
                   >
-                    {submitted ? (
-                      <>
-                        <MagnusText fontSize={15} color="white">
-                          Sent <Icon name="check" color="white" ml="md" />
-                        </MagnusText>
-                      </>
-                    ) : (
-                      "Send magic link"
-                    )}
+                    Sign in
+                  </Button>
+                  <Button
+                    block
+                    mt="lg"
+                    color="green700"
+                    bg="transparent"
+                    borderColor="green700"
+                    disabled={loading}
+                    borderWidth={1.5}
+                    onPress={() => {
+                      signUpWithEmail().finally(() => {
+                        setSubmitted(true);
+                      });
+                    }}
+                  >
+                    Sign up
                   </Button>
                   {submitted && (
                     <MagnusText color="gray900" fontSize="sm" mt="lg" mb="md">
